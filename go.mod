dabe00ceba00abc81dc6d8c56434fcdd04721922
cynic <daniel@szymanski.cc>
2022-06-19 20:49:10 +0200

    Switch from termui to tview as this as input edits etc

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.18
 
 require (
 	github.com/alexflint/go-arg v1.4.3
-	github.com/gizak/termui/v3 v3.1.0
+	github.com/gdamore/tcell/v2 v2.5.1
+	github.com/rivo/tview v0.0.0-20220610163003-691f46d6f500
 	github.com/stretchr/testify v1.7.2
 	github.com/suyashkumar/dicom v1.0.5
 )
@@ -12,10 +13,13 @@ require (
 require (
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/mattn/go-runewidth v0.0.2 // indirect
-	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/text v0.3.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/alexflint/go-arg"
@@ -10,8 +9,7 @@ import (
 	"github.com/suyashkumar/dicom/pkg/tag"
 	"github.com/suyashkumar/dicom/pkg/vrraw"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"github.com/rivo/tview"
 )
 
 var (
@@ -25,26 +23,12 @@ type args struct {
 
 func (args) Version() string { return "Version " + version + " (" + commit + ")" }
 
-
-// tree nodes
-type nodeValue string
-
-func (nv nodeValue) String() string {
-	return string(nv)
-}
-
 func main() {
 	var args args
 	p := arg.MustParse(&args)
 	if args.Input == "" {
 		p.Fail("missing DICOM input file")
 	}
-
-	// init UI
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
-	}
-	defer ui.Close()
 
 	dataset, err := dicom.ParseFile(args.Input, nil)
 	if err != nil {
@@ -53,15 +37,18 @@ func main() {
 	}
 
 	// create tree nodes with dicom tags
-	var nodes []*widgets.TreeNode
-	var currentGroupNode *widgets.TreeNode
+	rootDir := "."
+	root := tview.NewTreeNode(rootDir)
+	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
+	var currentGroupNode *tview.TreeNode
 	var currentGroup uint16
 	for _, e := range dataset.Elements {
 		if currentGroup != e.Tag.Group {
 			currentGroup = e.Tag.Group
 			groupTagText := fmt.Sprintf("%04x", e.Tag.Group)
-			currentGroupNode = &widgets.TreeNode{Value: nodeValue(groupTagText), Nodes: []*widgets.TreeNode{}}
-			nodes = append(nodes, currentGroupNode)
+			currentGroupNode = tview.NewTreeNode(groupTagText).SetSelectable(true)
+			root.AddChild(currentGroupNode)
+			//fmt.Printf("%s\n", groupTagText)
 		}
 
 		var tagName string
@@ -73,73 +60,19 @@ func main() {
 		if e.RawValueRepresentation != vrraw.Sequence && e.ValueLength < 150 {
 			value = e.Value.String()
 		}
-		elementText := fmt.Sprintf("\t%04x %s (%s): %s\n", e.Tag.Element, tagName, e.RawValueRepresentation, value)
-		elementNode := &widgets.TreeNode{Value: nodeValue(elementText), Nodes: nil}
-		currentGroupNode.Nodes = append(currentGroupNode.Nodes, elementNode)
+		elementText := fmt.Sprintf("\t%04x %s (%s): %s", e.Tag.Element, tagName, e.RawValueRepresentation, value)
+		elementNode := tview.NewTreeNode(elementText).SetSelectable(true)
+		currentGroupNode.AddChild(elementNode)
+		//fmt.Printf("\t%s\n", elementText)
 	}
 
-	l := widgets.NewTree()
-	l.TextStyle = ui.NewStyle(ui.ColorYellow)
-	l.WrapText = false
-	l.SetNodes(nodes)
+	tree.SetSelectedFunc(func(node *tview.TreeNode) {
+		node.SetExpanded(!node.IsExpanded())
+	})
 
-	x, y := ui.TerminalDimensions()
-
-	l.SetRect(0, 0, x, y)
-
-	ui.Render(l)
-
-	previousKey := ""
-	uiEvents := ui.PollEvents()
-	for {
-		e := <-uiEvents
-		switch e.ID {
-		case "q", "<C-c>":
-			return
-		case "j", "<Down>":
-			l.ScrollDown()
-		case "k", "<Up>":
-			l.ScrollUp()
-		case "<C-d>":
-			l.ScrollHalfPageDown()
-		case "<C-u>":
-			l.ScrollHalfPageUp()
-		case "<C-f>":
-			l.ScrollPageDown()
-		case "<C-b>":
-			l.ScrollPageUp()
-		case "g":
-			if previousKey == "g" {
-				l.ScrollTop()
-			}
-		case "<Home>":
-			l.ScrollTop()
-		case "<Enter>":
-			l.ToggleExpand()
-		case "l":
-			l.Expand()
-		case "h":
-			l.Collapse()
-		case "G", "<End>":
-			l.ScrollBottom()
-		case "E":
-			l.ExpandAll()
-		case "C":
-			l.CollapseAll()
-		case "<Resize>":
-			x, y := ui.TerminalDimensions()
-			l.SetRect(0, 0, x, y)
-		}
-
-		if previousKey == "g" {
-			previousKey = ""
-		} else {
-			previousKey = e.ID
-		}
-
-		ui.Render(l)
+	if err := tview.NewApplication().SetRoot(tree, true).Run(); err != nil {
+		panic(err)
 	}
-
 }
 
 func myFunc(param int) int {
