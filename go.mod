2ffb377febc45356c259650e84e513a988466062
cynic <daniel@szymanski.cc>
2022-06-19 18:05:09 +0200

    Add termui and render DICOM tags as tree

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/alexflint/go-arg v1.4.3
+	github.com/gizak/termui/v3 v3.1.0
 	github.com/stretchr/testify v1.7.2
 	github.com/suyashkumar/dicom v1.0.5
 )
@@ -11,6 +12,9 @@ require (
 require (
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/text v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/alexflint/go-arg"
 	"github.com/suyashkumar/dicom"
 	"github.com/suyashkumar/dicom/pkg/tag"
 	"github.com/suyashkumar/dicom/pkg/vrraw"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 var (
@@ -21,6 +25,14 @@ type args struct {
 
 func (args) Version() string { return "Version " + version + " (" + commit + ")" }
 
+
+// tree nodes
+type nodeValue string
+
+func (nv nodeValue) String() string {
+	return string(nv)
+}
+
 func main() {
 	var args args
 	p := arg.MustParse(&args)
@@ -28,17 +40,28 @@ func main() {
 		p.Fail("missing DICOM input file")
 	}
 
-	dataset, err := dicom.ParseFile(args.Input, nil) 
+	// init UI
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	dataset, err := dicom.ParseFile(args.Input, nil)
 	if err != nil {
 		fmt.Printf("Could not read DICOM file: '%s'\n", err.Error())
 		os.Exit(1)
 	}
 
+	// create tree nodes with dicom tags
+	var nodes []*widgets.TreeNode
+	var currentGroupNode *widgets.TreeNode
 	var currentGroup uint16
 	for _, e := range dataset.Elements {
 		if currentGroup != e.Tag.Group {
 			currentGroup = e.Tag.Group
-			fmt.Printf("%04x\n", currentGroup)
+			groupTagText := fmt.Sprintf("%04x", e.Tag.Group)
+			currentGroupNode = &widgets.TreeNode{Value: nodeValue(groupTagText), Nodes: []*widgets.TreeNode{}}
+			nodes = append(nodes, currentGroupNode)
 		}
 
 		var tagName string
@@ -50,8 +73,69 @@ func main() {
 		if e.RawValueRepresentation != vrraw.Sequence && e.ValueLength < 150 {
 			value = e.Value.String()
 		}
-		fmt.Printf("\t%04x %s (%s): %s\n", e.Tag.Element, tagName, e.RawValueRepresentation, value)
+		elementText := fmt.Sprintf("\t%04x %s (%s): %s\n", e.Tag.Element, tagName, e.RawValueRepresentation, value)
+		elementNode := &widgets.TreeNode{Value: nodeValue(elementText), Nodes: nil}
+		currentGroupNode.Nodes = append(currentGroupNode.Nodes, elementNode)
 	}
+
+	l := widgets.NewTree()
+	l.TextStyle = ui.NewStyle(ui.ColorYellow)
+	l.WrapText = false
+	l.SetNodes(nodes)
+
+	x, y := ui.TerminalDimensions()
+
+	l.SetRect(0, 0, x, y)
+
+	ui.Render(l)
+
+	previousKey := ""
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			l.ScrollDown()
+		case "k", "<Up>":
+			l.ScrollUp()
+		case "<C-d>":
+			l.ScrollHalfPageDown()
+		case "<C-u>":
+			l.ScrollHalfPageUp()
+		case "<C-f>":
+			l.ScrollPageDown()
+		case "<C-b>":
+			l.ScrollPageUp()
+		case "g":
+			if previousKey == "g" {
+				l.ScrollTop()
+			}
+		case "<Home>":
+			l.ScrollTop()
+		case "<Enter>":
+			l.ToggleExpand()
+		case "G", "<End>":
+			l.ScrollBottom()
+		case "E":
+			l.ExpandAll()
+		case "C":
+			l.CollapseAll()
+		case "<Resize>":
+			x, y := ui.TerminalDimensions()
+			l.SetRect(0, 0, x, y)
+		}
+
+		if previousKey == "g" {
+			previousKey = ""
+		} else {
+			previousKey = e.ID
+		}
+
+		ui.Render(l)
+	}
+
 }
 
 func myFunc(param int) int {
