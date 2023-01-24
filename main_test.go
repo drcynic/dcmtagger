8dfdc9e7468b5f2c128f9cfcef80ce7679d07a49
cynic <daniel@szymanski.cc>
2023-01-24 23:09:52 +0100

    Added go action setup and fixed tests to run

--- main_test.go
+++ main_test.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSomething(t *testing.T) {
-	fmt.Println("aber hallo")
-
 	assert := assert.New(t)
 
 	// assert equality
-	input := 12
-	assert.Equal(16, myFunc(input), "they should be equal")
+	input := 16
+	assert.Equal(16, input, "just a test test")
 }
